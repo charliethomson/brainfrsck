576c435045ace5ccfe2dcedc72492bce96bae5a2
Charlie Thomson <charliecthomson@gmail.com>
2019-10-26 01:38:51 -0400

    remove examples

--- examples/from_string.rs
+++
@@ -1,8 +0,0 @@
-
-use brainfrsck::prelude::*;
-
-fn main() {
-    let sum: &'static str = ",>,<[[[->+<],],]";
-
-    eprintln!("{}", eval_string(sum, Some((1..6).collect())).unwrap().to_vec()[0]);
-}
